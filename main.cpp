61a5739d61bd78a217b70ab643b2951bda7652ba
40two <dimitris.mpouzas@gmail.com>
2014-04-10 22:45:45 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,9 +11,9 @@ auto renderer_ptr = std::make_shared<Renderer>(0.0f, 0.3f, 0.4f, 1.0f);
 //! \return void.
 void
 display(void)
-	{
+  {
   (*renderer_ptr)();
-	}
+  }
 
 //! \brief
 //! - Main function.
@@ -25,13 +25,14 @@ display(void)
 int
 main(int argc, char *argv[])
   {
-	glutInit(&argc, argv);
+  glutInit(&argc, argv);
   glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH);
-	glutInitWindowSize(512, 512);
-	glutInitContextVersion(4, 2);
-	glutInitContextProfile(GLUT_CORE_PROFILE);
-	glutCreateWindow(argv[0]);
+  glutInitWindowSize(512, 512);
+  glutInitContextVersion(4, 2);
+  glutInitContextProfile(GLUT_CORE_PROFILE);
+  glutCreateWindow(argv[0]);
   glutDisplayFunc(display);
-	glutMainLoop();
-	return 0;
+  glutMainLoop();
+
+  return 0;
   }
